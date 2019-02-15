adfa301dc8d0e5ab8787f662c0a85325bdb8a045
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-15 11:17:02 -0600

    Added the end-points for posting an action and project

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
         res.status(200).json(actions);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ error: "The projects information could not be retrieved." });
+        res.status(500).json({ error: "The actions information could not be retrieved." });
     }
 });
 
@@ -21,8 +21,28 @@ router.get('/:id', async (req, res) => {
         res.status(200).json(action);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ error: "The projects information could not be retrieved." });
+        res.status(500).json({ error: "The actions information could not be retrieved." });
     }
 });
 
+router.post('/', async (req, res) => {
+    if(!req.body.notes || !req.body.description){
+        res.status(400).json({ errorMessage: 'Please provide notes and description for the action' });
+    } else {
+         try {
+            if(req.headers.projectid){
+                const newAction = { ...req.body, project_id: req.headers.projectid};
+                const action = await Actions.insert(newAction);
+ 
+                res.status(201).json(action);
+            } else {
+                res.status(404).json({ errorMessage: 'Requires id of the project the actions belong to' });
+            }
+         } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: "There was an error while saving the action to the database" });
+         }
+    }
+ });
+ 
 module.exports = router;
